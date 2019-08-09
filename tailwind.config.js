1d949e550b6ddb4276580fef2fa4bff83f4af080
francisudeji <francis.udejiofficial@gmail.com>
2019-08-09 05:17:39 +0100

    Changed default screen settings and container style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   theme: {
-    extend: {}
+    container: {
+      center: true
+    },
+    backgroundColor: theme => ({
+      primary: '#15202b',
+      secondary: '#2d3748'
+    }),
+    screens: {
+      sm: '576px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '960px'
+      // => @media (min-width: 1024px) { ... }
+    }
   },
-  variants: {},
+  variants: {
+    // backgroundColor: ['hover']
+  },
   plugins: []
 }
