56daa1707412afd4372977f1bf0ea60315bbad9d
francisudeji <francis.udejiofficial@gmail.com>
2019-08-26 15:59:18 +0100

    Added personal information for blog pages

---
+++ blog.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMeta: {
+    title: 'Francis Udeji | Blog',
+    author: 'Francis Udeji',
+    image: '/static/francisudeji.webp',
+    description: 'Articles on JavaScript and Web Development Topics',
+    siteUrl: 'https://francisudeji.com',
+    social: {
+      twitter: 'francisudeji'
+    },
+    postsPerPage: 5
+  }
+}
