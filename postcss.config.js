b802cdc38082091ace4781ce28cac7227d9d298c
francisudeji <francis.udejiofficial@gmail.com>
2020-05-02 11:58:08 +0100

    Update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,9 @@ module.exports = {
   plugins: [
     require('postcss-easy-import'),
     require('tailwindcss'),
-    require('autoprefixer')
-  ]
-}
+    require('autoprefixer'),
+  ],
+};
+// module.exports = {
+//   plugins: ['postcss-easy-import', 'tailwindcss', 'autoprefixer'],
+// };
