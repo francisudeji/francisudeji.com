f22eaffcff5361735dbbf275bac3bba0eab90879
francisudeji <francis.udejiofficial@gmail.com>
2020-05-03 11:27:33 +0100

    Update tailwind package and add purge css to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-  theme: {
-    container: {
-      center: true
-    },
-    extend: {
-      width: {
-        '48%': '48%'
-      },
-      colors: {
-        primary: '#15202b',
-        secondary: '#2d3748'
-      }
-    },
-    screens: {
-      sm: '576px',
-      md: '768px',
-      lg: '960px'
-    }
-  },
-  variants: {},
-  plugins: []
-}
+	purge: ['./pages/**/*.js', './components/**/*.js'],
+	theme: {
+		container: {
+			center: true,
+		},
+		extend: {
+			width: {
+				'48%': '48%',
+			},
+			colors: {
+				primary: '#15202b',
+				secondary: '#2d3748',
+			},
+		},
+		screens: {
+			sm: '576px',
+			md: '768px',
+			lg: '960px',
+		},
+	},
+	variants: {},
+	plugins: [],
+};
